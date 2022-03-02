5fee5a58a858bfe5574003c8e6c3dd3740ffdf81
DEVESH SINGH <76179606+Devesh1602@users.noreply.github.com>
2022-03-02 13:21:25 +0530

    Add files via upload

---
+++ android/support/p010v4/app/RemoteActionCompatParcelizer.java
@@ -0,0 +1,15 @@
+package android.support.p010v4.app;
+
+import androidx.core.app.RemoteActionCompat;
+import p164r1.C3037a;
+
+/* renamed from: android.support.v4.app.RemoteActionCompatParcelizer */
+public final class RemoteActionCompatParcelizer extends androidx.core.app.RemoteActionCompatParcelizer {
+    public static RemoteActionCompat read(C3037a aVar) {
+        return androidx.core.app.RemoteActionCompatParcelizer.read(aVar);
+    }
+
+    public static void write(RemoteActionCompat remoteActionCompat, C3037a aVar) {
+        androidx.core.app.RemoteActionCompatParcelizer.write(remoteActionCompat, aVar);
+    }
+}
